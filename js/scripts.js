707fe91f636b4feac85419d3422a5f0aa8831dc1
Joshua <joshuaatteberry@gmail.com>
2015-02-09 10:39:52 -0800

    remove the 'not' text when submitting a leap year after a not leap year

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
@@ -8,12 +8,15 @@ var leapYear = function(year) {
 
 $(function() {
   $("form#leap-year").submit(function(event) {
+
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
 
     $(".year").text(year);
     if (!result) {
       $(".not").text("not");
+    } else {
+      $(".not").text(""); // This removes the 'not' text.
     }
 
     $("#result").show();
